7d44df3385edbc289c1949310e69d22712b2c39c
sby051 <91495085+sby051@users.noreply.github.com>
2023-03-10 00:35:33 +0000

    update

--- ci.sh
+++ ci.sh
@@ -23,17 +23,31 @@ fi
 
 # if we get here, then the version is in the changelog
 
-# we want to see if there are any commited changes to the working tree
-# if there are, then we want to fail the build
-# if there are not, then we want to continue
-if [ -n "$(git status --porcelain)" ]; then
-  echo "[CI-ALERT] :: [$(date +%x)] -> There are uncommitted changes in the working tree.
-[!] CI cannot continue without a clean working tree.
-[!] Please commit your changes and try again.
+# we want to see if the current repo is equal to the repo in the package.json file
+# if it is, then we can continue
+# if it is not, then we cannot continue
+# this is to prevent a malicious user from changing the package.json file to point to their own repo
+# and then running the CI script to publish to their own repo
+
+# get the repo name from the package.json file
+repo=$(cat package.json | grep name | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
+
+# get the repo name from the current repo
+current_repo=$(git remote -v | grep fetch | head -1 | awk '{ print $2 }' | sed 's/.*\///g' | sed 's/.git//g')
+
+# if the repo names do not match, then fail
+if [ "$repo" != "$current_repo" ]; then
+  echo "[CI-ALERT] :: [$(date +%x)] -> Repo name in package.json ($repo) does not match current repo ($current_repo)
+[!] CI cannot continue without a matching repo name in the package.json file.
+[!] Please update the package.json file and try again.
 [!] Exiting with error code 1.
   "
   exit 1
 fi
+
+# if we get here, then the repo names match
+
+# we can continue
 
 echo "Version in package.json ($version) matches latest entry in CHANGELOG.md ($latest)
 
